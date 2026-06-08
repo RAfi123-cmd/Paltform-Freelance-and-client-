a8a822566f6ed9aeaa255615dab9283a8c29f435
Rafi123-CMD <rafiulhuda27@gmail.com>
2026-06-08 23:44:43 +0700

    bikin method ORM setiap model

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,23 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function wallet(){
+        return $this->hasOne(Wallet::class);
+    }
+
+    public function projects(){
+        return $this->hasMany(Project::class, 'client_id', 'id')->orderByDesc('id');
+        // $user->projects() ... menampilkan seluruh project dari user tersebut
+    }
+
+    public function proposals(){
+        return $this->hasMany(ProjectApplication::class, 'freelancer_id', 'id')->orderByDesc('id');
+    }
+
+    public function hasAppliedToProject($projectId){
+        return ProjectApplication::where('project_id', $projectId)
+        ->where('freelancer_id', $this->id)
+        ->exists();
+    }
 }
